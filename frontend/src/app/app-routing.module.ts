26315aadf8889cb64d4482dccfdb6916caca56e1
Erick <erickjhorman@gmail.com>
2019-05-21 07:06:01 -0500

    Se crearon rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/auth/login_principal/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { DashboardComponent } from './components/auth/dashboard/dashboard.component';
+import { RequestResetComponent } from './components/auth/password-reset/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/auth/password-reset/response-reset/response-reset.component';
 
-const routes: Routes = [];
+const appRoutesAuth: Routes = [
+  {
+    path:'login',
+    component:LoginComponent 
+  },
+  {
+    path:'signup',
+    component:SignupComponent 
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent 
+  },
+  {
+    path:'request-password-reset',
+    component:RequestResetComponent 
+  },
+  {
+    path:'response-password-reset',
+    component:ResponseResetComponent 
+  },
+];
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutesAuth)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
